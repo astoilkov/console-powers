a366ec39482d039509bc514b6b204369e72bd1c5
Antonio Stoilkov <antonio.stoilkov@gmail.com>
2015-05-21 14:21:48 +0300

    comment code and implement console.image

--- README.md
+++ README.md
@@ -4,8 +4,7 @@ Create beautiful console messages and impress your users
 
 ## Used in [jsblocks](http://jsblocks.com)
 
-console.message code is used in [jsblocks](http://jsblocks.com) and achieves amazing debugging experience for the developers using it.
-Take a look at the gif below:
+console.message code is used in [jsblocks](http://jsblocks.com) and achieves amazing debugging experience for the developers using it
 
 ![console.message example](http://jsblocks.com/img/debugging.gif)
 
@@ -15,13 +14,30 @@ Take a look at the gif below:
 
 ```javascript
 console.message()
-	.addSpan('Aruments missmatch:', { color: 'yellow' })
+	.addSpan('Arguments mismatch:', { background: 'yellow' })
 	.addText(' ')
-	.addSpan('addTodo()', { color: '#333' })
+	.addSpan('addTodo()', { background: '#ccc' })
 	.addText(' - ')
 	.addSpan('less arguments than the required specified', { color: 'red' })
 	.print();
 ```
+
+![arguments mismatch example](https://dl.dropboxusercontent.com/u/4277603/console.message/arguments-mismatch-example.png)
+
+### Grouping awesomeness
+
+```javascript
+console.message()
+	.beginGroup()
+	.addSpan('Error occurred while executing code', { color: 'red', fontSize: 24 })
+		.beginGroup(true)
+		.addSpan('For more information expand the group', { color: 'green' })
+		.newLine()
+		.addText('Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.')
+		.print();
+```
+
+![grouping awesomeness example](https://dl.dropboxusercontent.com/u/4277603/console.message/grouping-awesomeness-example.png)
 
 ## Browser support
 
@@ -34,30 +50,106 @@ However, it will print messages without styles when they are not supported.
 
 Appends a text to the current message. All styles in the current span are applied.
 
+```javascript
+console.message().addText('this is equal to just calling console.log()').print();
+
+console.message()
+	.beginSpan({ color: 'red' })
+	.addText('Error: ')
+	.endSpan()
+	.addText('this is more useful scenario')
+	.print();
+```
+
+![addText() example](https://dl.dropboxusercontent.com/u/4277603/console.message/addText-example.png)
+
 ### addSpan(text:String, styles:Object)
 
 Appends a text with particular style. Styles is an object containing CSS properties.
+
+```javascript
+console.message()
+	.addSpan('This is green!', { color: 'green'})
+	.addSpan('This is red!', { color: 'red'})
+	.print();
+```
+
+![addSpan() example](https://dl.dropboxusercontent.com/u/4277603/console.message/addSpan-example.png)
 
 ### beginSpan(styles:Object)
 
 Starts a span with particular style and all appended text after it will use the style.
 
+```javascript
+console.message()
+	.beginSpan({ color: 'red' })
+	.addText('this is red! ')
+		.beginSpan({ color: 'green' })
+		.addText('this is green! ')
+		.endSpan()
+	.addText('this is again red!')
+	.endSpan()
+	.print();
+```
+
+![beginSpan() example](https://dl.dropboxusercontent.com/u/4277603/console.message/beginSpan-example.png)
+
 ### endSpan()
 
-Ends the current span styles and backs to the previous styles or the root if there no other parents.
+Ends the current span styles and backs to the previous styles or the root if there are no other parents.
+Take a look at the example above.
 
 ### beginGroup(collapsed:Boolean = false)
 
 Begins a group. By default the group is expanded. Provide true if you want the group to be collapsed.
 
+```javascript
+console.message()
+	// pass true if you want to create a collapsed group
+	.beginGroup()
+	.addText('The group header text')
+	.newLine()
+	.addText('The group content')
+	.endGroup()
+	.print();
+```
+
+![beginGroup() example](https://dl.dropboxusercontent.com/u/4277603/console.message/beginGroup-example.png)
+
 ### endGroup()
 
 Ends the group and returns to writing to the parent message.
+
+```javascript
+console.message()
+		.beginGroup(true)
+		.addText('group')
+		.endGroup()
+	.addText('Outside of group')
+	.print();
+```
+
+![endGroup() example](https://dl.dropboxusercontent.com/u/4277603/console.message/endGroup-example.png)
 
 ### newLine()
 
 Adds a new line to the output.
 
+```javascript
+console.message()
+	.addText('first line')
+	.newLine()
+	.addText('second line')
+	.print();
+```
+
+![newLine() example](https://dl.dropboxusercontent.com/u/4277603/console.message/newLine-example.png)
+
 ### print()
 
 Prints the message to the console.
+Every message chaining should end with print. No print() call no message.
+
+```javascript
+console.message().addText('just text').print();
+```
--- console.message.js
+++ console.message.js
@@ -1,233 +1,369 @@
 (function () {
-	function ConsoleMessage() {
-	  if (!ConsoleMessage.prototype.isPrototypeOf(this)) {
-	    return new ConsoleMessage();
-	  }
-	  this._rootSpan = {
-	    styles: {},
-	    children: [],
-	    parent: null
-	  };
-	  this._currentSpan = this._rootSpan;
-	}
-	
-	ConsoleMessage.Support = (function () {
-	  // Taken from https://github.com/jquery/jquery-migrate/blob/master/src/core.js
-	  function uaMatch( ua ) {
-	    ua = ua.toLowerCase();
-	
-	    var match = /(chrome)[ \/]([\w.]+)/.exec( ua ) ||
-	      /(webkit)[ \/]([\w.]+)/.exec( ua ) ||
-	      /(opera)(?:.*version|)[ \/]([\w.]+)/.exec( ua ) ||
-	      /(msie) ([\w.]+)/.exec( ua ) ||
-	      ua.indexOf("compatible") < 0 && /(mozilla)(?:.*? rv:([\w.]+)|)/.exec( ua ) ||
-	      [];
-	
-	    return {
-	      browser: match[ 1 ] || "",
-	      version: match[ 2 ] || "0"
-	    };
-	  }
-	  var browserData = uaMatch(navigator.userAgent);
-	
-	  return {
-	    isIE: browserData.browser == 'msie' || (browserData.browser == 'mozilla' && parseInt(browserData.version, 10) == 11)
-	  };
-	})();
-	
-	ConsoleMessage.prototype = {
-	  beginGroup: function () {
-	    this._currentSpan.children.push({
-	      type: 'group',
-	      parent: this._currentSpan
-	    });
-	    return this;
-	  },
-	
-	  beginCollapsedGroup: function () {
-	    this._currentSpan.children.push({
-	      type: 'groupCollapsed'
-	    });
-	    return this;
-	  },
-	
-	  endGroup: function () {
-	    this._currentSpan.children.push({
-	      type: 'groupEnd',
-	      parent: this._currentSpan
-	    });
-	    return this;
-	  },
-	
-	  beginSpan: function (styles) {
-	    var span = {
-	      type: 'span',
-	      styles: styles,
-	      children: [],
-	      parent: this._currentSpan
-	    };
-	    this._currentSpan.children.push(span);
-	    this._currentSpan = span;
-	    return this;
-	  },
-	
-	  endSpan: function () {
-	    this._currentSpan = this._currentSpan.parent || this._currentSpan;
-	    return this;
-	  },
-	  
-	  addSpan: function (text, styles) {
-	    this.beginSpan(styles);
-	    this.addText(text);
-	    this.endSpan();
-	    return this;
-	  },
-	
-	  addText: function (message) {
-	    this._currentSpan.children.push({
-	      type: 'text',
-	      message: message,
-	      parent: this._currentSpan
-	    });
-	    return this;
-	  },
-	
-	  newLine: function (type) {
-	    this._currentSpan.children.push({
-	      type: type || 'log',
-	      parent: this._currentSpan
-	    });
-	    return this;
-	  },
-	
-	  addImage: function () {
-	    (function () {
-	      var faviconUrl = "http://d2c87l0yth4zbw.cloudfront.net/i/_global/favicon.png",
-	        css = "background-image: url('" + faviconUrl + "');" +
-	          "background-repeat: no-repeat;" +
-	          "display: block;" +
-	          "background-size: 13px 13px;" +
-	          "padding-left: 13px;" +
-	          "margin-left: 5px;",
-	        text = "Do you like coding? Visit www.spotify.com/jobs";
-	      if (navigator.userAgent.match(/chrome/i)) {
-	        console.log(text + '%c', css);
-	      } else {
-	        console.log('%c   ' + text, css);
-	      }
-	    })();
-	    return this;
-	  },
-	
-	  addElement: function (element) {
-	    this._currentSpan.children.push({
-	      type: 'element',
-	      element: element,
-	      parent: this._currentSpan
-	    });
-	    return this;
-	  },
-	
-	  print: function () {
-	    if (typeof console == 'undefined') {
-	      return;
-	    }
-	
-	    var messages = [this._newMessage()];
-	    var message;
-	
-	    this._printSpan(this._rootSpan, messages);
-	
-	    for (var i = 0; i < messages.length; i++) {
-	      message = messages[i];
-	      if (message.text && message.text != '%c' && console[message.type]) {
-	        Function.prototype.apply.call(console[message.type], console, [message.text].concat(message.args));
-	      }
-	    }
-	
-	    return this;
-	  },
-	
-	  _printSpan: function (span, messages) {
-	    var children = span.children;
-	    var message = messages[messages.length - 1];
-	
-	    this._addSpanData(span, message);
-	
-	    for (var i = 0; i < children.length; i++) {
-	      this._handleChild(children[i], messages);
-	    }
-	  },
-	
-	  _handleChild: function (child, messages) {
-	    var message = messages[messages.length - 1];
-	
-	    switch (child.type) {
-	      case 'group':
-	        messages.push(this._newMessage('group'));
-	        break;
-	      case 'groupCollapsed':
-	        messages.push(this._newMessage('groupCollapsed'));
-	        break;
-	      case 'groupEnd':
-	        message = this._newMessage('groupEnd');
-	        message.text = ' ';
-	        messages.push(message);
-	        messages.push(this._newMessage())
-	        break;
-	      case 'span':
-	        this._printSpan(child, messages);
-	        this._addSpanData(child, message);
-	        this._addSpanData(child.parent, message);
-	        break;
-	      case 'text':
-	        message.text += child.message;
-	        break;
-	      case 'element':
-	        message.text += '%o';
-	        message.args.push(child.element);
-	        break;
-	      case 'log':
-	        messages.push(this._newMessage(child.type));
-	        break;
-	    }
-	  },
-	
-	  _addSpanData: function (span, message) {
-	    if (!ConsoleMessage.Support.isIE) {
-	      if (message.text.substring(message.text.length - 2) == '%c') {
-	        message.args[message.args.length - 1] = this._stylesString(span.styles);
-	      } else {
-	        message.text += '%c';
-	        message.args.push(this._stylesString(span.styles));
-	      }
-	    }
-	  },
-	
-	  _newMessage: function (type) {
-	    return {
-	      type: type || 'log',
-	      text: '',
-	      args: []
-	    };
-	  },
-	
-	  _stylesString: function (styles) {
-	    var result = '';
-	    for (var key in styles) {
-	      result += key + ':' + styles[key] + ';';
-	    }
-	    return result;
-	  }
-	};
-	
-	if (typeof window != 'undefined') {
-		if (!window.console) {
-			window.console = {};
-		}
-		
-		window.console.message = function () {
-			return new ConsoleMessage();
-		};
-	}
+  function ConsoleMessage() {
+    if (!ConsoleMessage.prototype.isPrototypeOf(this)) {
+      return new ConsoleMessage();
+    }
+    this._rootSpan = {
+      styles: {},
+      children: [],
+      parent: null
+    };
+    this._currentSpan = this._rootSpan;
+    this._waiting = 0;
+    this._readyCallback = null;
+  }
+
+  ConsoleMessage.prototype = {
+    /**
+     * Begins a group. By default the group is expanded. Provide true if you want the group to be collapsed.
+     * @returns {ConsoleMessage} - Returns the message object itself to allow chaining.
+     */
+    beginGroup: function (collapsed) {
+      this._currentSpan.children.push({
+        type: collapsed === true ? 'groupCollapsed' : 'group',
+        parent: this._currentSpan
+      });
+      return this;
+    },
+
+    /**
+     * Ends the group and returns to writing to the parent message.
+     * @returns {ConsoleMessage} - Returns the message object itself to allow chaining.
+     */
+    endGroup: function () {
+      this._currentSpan.children.push({
+        type: 'groupEnd',
+        parent: this._currentSpan
+      });
+      return this;
+    },
+
+    /**
+     * Starts a span with particular style and all appended text after it will use the style.
+     * @param {Object} styles - The CSS styles to be applied to all text until endSpan() is called
+     * @returns {ConsoleMessage} - Returns the message object itself to allow chaining.
+     */
+    beginSpan: function (styles) {
+      var span = {
+        type: 'span',
+        styles: styles,
+        children: [],
+        parent: this._currentSpan
+      };
+      this._currentSpan.children.push(span);
+      this._currentSpan = span;
+      return this;
+    },
+
+    /**
+     * Ends the current span styles and backs to the previous styles or the root if there are no other parents.
+     * @returns {ConsoleMessage} - Returns the message object itself to allow chaining.
+     */
+    endSpan: function () {
+      this._currentSpan = this._currentSpan.parent || this._currentSpan;
+      return this;
+    },
+
+    /**
+     * Appends a text with particular style. Styles is an object containing CSS properties.
+     * @param {string} text - The text to be added.
+     * @param {Object} styles - Object with all styles that will be applied to the text.
+     * @returns {ConsoleMessage} - Returns the message object itself to allow chaining.
+     */
+    addSpan: function (text, styles) {
+      this.beginSpan(styles);
+      this.addText(text);
+      this.endSpan();
+      return this;
+    },
+
+    /**
+     * Appends a text to the current message. All styles in the current span are applied.
+     * @param {string} text - The text to be appended
+     * @returns {ConsoleMessage} - Returns the message object itself to allow chaining.
+     */
+    addText: function (text) {
+      this._currentSpan.children.push({
+        type: 'text',
+        message: text,
+        parent: this._currentSpan
+      });
+      return this;
+    },
+
+    /**
+     * Adds a new line to the output.
+     * @returns {ConsoleMessage} - Returns the message object itself to allow chaining.
+     */
+    newLine: function (type) {
+      this._currentSpan.children.push({
+        type: type || 'log',
+        parent: this._currentSpan
+      });
+      return this;
+    },
+
+    /**
+     * Adds an image to the output.
+     * @param {string} url - The url location of the image.
+     * @param {number}
+     * @returns {ConsoleMessage} - Returns the message object itself to allow chaining.
+     */
+    addImage: function (url, scale) {
+      var _this = this;
+      var image = new Image();
+      var styles = {
+        backgroundImage: 'url(' + url + ')',
+        backgroundRepeat: 'no-repeat',
+        color: 'transparent',
+        fontSize: 1,
+        paddingLeft: 13,
+        marginLeft: 5
+      };
+
+      scale = typeof scale == 'number' ? scale : 1;
+
+      this.addSpan(' ', styles);
+
+      this._wait();
+
+      image.onload = function () {
+        var width = this.width * scale;
+        var height = this.height * scale;
+
+        styles.backgroundSize = (width + 'px') + ' ' + (height + 'px');
+        styles.padding = (height / 2) + 'px ' + (width / 2) + 'px';
+        styles.lineHeight = height;
+
+        _this._ready();
+      };
+
+      image.onerror = function () {
+        _this._ready();
+      };
+
+      image.src = url;
+
+      return this;
+    },
+
+    /**
+     * Adds an interactive DOM element to the output.
+     * @param {HTMLElement} element - The DOM element to be added.
+     * @returns {ConsoleMessage} - Returns the message object itself to allow chaining.
+     */
+    addElement: function (element) {
+      this._currentSpan.children.push({
+        type: 'element',
+        element: element,
+        parent: this._currentSpan
+      });
+      return this;
+    },
+
+    /**
+     * Prints the message to the console.
+     * Until print() is called there will be no result to the console.
+     * @returns {ConsoleMessage} - Returns a new ConsoleMessage instance.
+     */
+    print: function () {
+      if (typeof console == 'undefined') {
+        return;
+      }
+
+      this._onReady(this._print);
+
+      return new ConsoleMessage();
+    },
+
+    _print: function () {
+      var messages = [this._newMessage()];
+      var message;
+
+      this._printSpan(this._rootSpan, messages);
+
+      for (var i = 0; i < messages.length; i++) {
+        message = messages[i];
+        if (message.text && message.text != '%c' && console[message.type]) {
+          Function.prototype.apply.call(console[message.type], console, [message.text].concat(message.args));
+        }
+      }
+    },
+
+    _onReady: function (callback) {
+      if (this._waiting === 0) {
+        callback.call(this);
+      } else {
+        this._readyCallback = callback;
+      }
+    },
+
+    _wait: function () {
+      this._waiting += 1;
+    },
+
+    _ready: function () {
+      this._waiting -= 1;
+      if (this._waiting === 0 && this._readyCallback) {
+        this._readyCallback();
+      }
+    },
+
+    _printSpan: function (span, messages) {
+      var children = span.children;
+      var message = messages[messages.length - 1];
+
+      this._addSpanData(span, message);
+
+      for (var i = 0; i < children.length; i++) {
+        this._handleChild(children[i], messages);
+      }
+    },
+
+    _handleChild: function (child, messages) {
+      var message = messages[messages.length - 1];
+
+      switch (child.type) {
+        case 'group':
+          messages.push(this._newMessage('group'));
+          break;
+        case 'groupCollapsed':
+          messages.push(this._newMessage('groupCollapsed'));
+          break;
+        case 'groupEnd':
+          message = this._newMessage('groupEnd');
+          message.text = ' ';
+          messages.push(message);
+          messages.push(this._newMessage())
+          break;
+        case 'span':
+          this._printSpan(child, messages);
+          this._addSpanData(child, message);
+          this._addSpanData(child.parent, message);
+          break;
+        case 'text':
+          message.text += child.message;
+          break;
+        case 'element':
+          message.text += '%o';
+          message.args.push(child.element);
+          break;
+        case 'log':
+          messages.push(this._newMessage(child.type));
+          break;
+      }
+    },
+
+    _addSpanData: function (span, message) {
+      if (!ConsoleMessage.Support.isIE) {
+        if (message.text.substring(message.text.length - 2) == '%c') {
+          message.args[message.args.length - 1] = this._stylesString(span.styles);
+        } else {
+          message.text += '%c';
+          message.args.push(this._stylesString(span.styles));
+        }
+      }
+    },
+
+    _newMessage: function (type) {
+      return {
+        type: type || 'log',
+        text: '',
+        args: []
+      };
+    },
+
+    _stylesString: function (styles) {
+      var result = '';
+      var value;
+      var key;
+
+      for (key in styles) {
+        key = this._fixCssStyleKey(key);
+        value = styles[key];
+        if (typeof value === 'number' && !ConsoleMessage.CssNumbers[key]) {
+          value += 'px';
+        }
+        result += this._toDashKey(key) + ':' + value + ';';
+      }
+      return result;
+    },
+
+    _fixCssStyleKey: function (key) {
+      return key.replace(/-\w/g, function (match) {
+        return match.charAt(1).toUpperCase();
+      });
+    },
+
+    _toDashKey: function (key) {
+      return key.replace(/[A-Z]/g, function (match) {
+        return '-' + match.toLowerCase();
+      });
+    }
+  };
+
+  ConsoleMessage.CssNumbers = {
+    columnCount: true,
+    fillOpacity: true,
+    flexGrow: true,
+    flexShrink: true,
+    fontWeight: true,
+    lineHeight: true,
+    opacity: true,
+    order: true,
+    orphans: true,
+    widows: true,
+    zIndex: true,
+    zoom: true
+  };
+
+  ConsoleMessage.Support = (function () {
+    // Taken from https://github.com/jquery/jquery-migrate/blob/master/src/core.js
+    function uaMatch(ua) {
+      ua = ua.toLowerCase();
+
+      var match = /(chrome)[ \/]([\w.]+)/.exec(ua) ||
+        /(webkit)[ \/]([\w.]+)/.exec(ua) ||
+        /(opera)(?:.*version|)[ \/]([\w.]+)/.exec(ua) ||
+        /(msie) ([\w.]+)/.exec(ua) ||
+        ua.indexOf("compatible") < 0 && /(mozilla)(?:.*? rv:([\w.]+)|)/.exec(ua) || [];
+
+      return {
+        browser: match[1] || "",
+        version: match[2] || "0"
+      };
+    }
+    var browserData = uaMatch(navigator.userAgent);
+
+    return {
+      isIE: browserData.browser == 'msie' || (browserData.browser == 'mozilla' && parseInt(browserData.version, 10) == 11)
+    };
+  })();
+
+  if (typeof window != 'undefined') {
+    if (!window.console) {
+      window.console = {};
+    }
+
+    window.console.message = function () {
+      return new ConsoleMessage();
+    };
+
+    /**
+     * Logs
+     * @param {string} url - The url location of the image.
+     * @param {number}
+     */
+    window.console.image = function (url, scale) {
+      new ConsoleMessage().addImage(url, scale).print();
+    };
+
+    /**
+     * Outputs an interactive DOM element.
+     * @param {HTMLElement} element - The DOM element to be outputed.
+     */
+    window.console.element = function (element) {
+      new ConsoleMessage().addElement(element).print();
+    };
+  }
 })();
