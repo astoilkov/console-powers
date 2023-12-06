7b5ccbc43f15b50bf6cdfccfea4274056e03bad1
Antonio Stoilkov <hello@astoilkov.com>
2023-12-06 17:44:38 +0200

    ✨ single line and multi line object inspections

--- src/inspect/inspectors/inspectObject.ts
+++ src/inspect/inspectors/inspectObject.ts
@@ -2,24 +2,13 @@ import ConsoleMessage from "../../core/ConsoleMessage";
 import { consoleText } from "../../core/consoleText";
 import consoleStyles from "../consoleStyles";
 import inspectAny from "./inspectAny";
+import isPrimitive from "../../utils/isPrimitive";
 
 export default function inspectObject(value: object): ConsoleMessage[] {
-    return singleLineObject(value);
+    return Object.values(value).every(isPrimitive)
+        ? singleLineObject(value)
+        : multiLineObject(value);
 }
-
-// function multiLineObject(value: object): ConsoleMessage[] {
-//     const pad = maxKeyLength(value)
-//     const messages: ConsoleMessage[] = []
-//     for (const key in value) {
-//         // if (value.hasOwnProperty(key)) {
-//         messages.push(consoleText(key.padEnd(pad), consoleStyles.collapsedObjectKey))
-//         messages.push(consoleText(' : '))
-//         messages.push(...valueMessages(value[key as keyof typeof value]))
-//         messages.push(consoleText('\n'))
-//         // }
-//     }
-//     return messages
-// }
 
 function singleLineObject(value: object): ConsoleMessage[] {
     const messages: ConsoleMessage[] = [consoleText("{ ")];
@@ -43,12 +32,28 @@ function singleLineObject(value: object): ConsoleMessage[] {
     return messages;
 }
 
-// function maxKeyLength(object: object): number {
-//     let max = 0
-//     for (const key in object) {
-//         if (object.hasOwnProperty(key)) {
-//             max = Math.max(max, key.length)
-//         }
-//     }
-//     return max
-// }
+function multiLineObject(value: object): ConsoleMessage[] {
+    const maxLength = maxKeyLength(value);
+    console.log(value, maxLength)
+    const messages: ConsoleMessage[] = [];
+    for (const key in value) {
+        // if (value.hasOwnProperty(key)) {
+        messages.push(consoleText(key, consoleStyles.collapsedObjectKey));
+        messages.push(consoleText(": "));
+        messages.push(consoleText(" ".repeat(maxLength - key.length)))
+        messages.push(...inspectAny(value[key as keyof typeof value]));
+        messages.push(consoleText("\n"));
+        // }
+    }
+    return messages;
+}
+
+function maxKeyLength(object: object): number {
+    let max = 0;
+    for (const key in object) {
+        // if (object.hasOwnProperty(key)) {
+        max = Math.max(max, key.length);
+        // }
+    }
+    return max;
+}
