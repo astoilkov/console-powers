467b98aa5cf07167c15d894553dc05e1ea451974
Antonio Stoilkov <hello@astoilkov.com>
2023-12-06 14:51:16 +0200

    🔀 objectMessages() to use consoleTable() under the hood

--- src/inspect/inspectors/objectMessages.ts
+++ src/inspect/inspectors/objectMessages.ts
@@ -1,26 +1,52 @@
-import valueMessages from "./valueMessages";
-import consoleStyles from "../consoleStyles";
-import { consoleText } from "../../core/consoleText";
 import ConsoleMessage from "../../core/ConsoleMessage";
+import consoleTable from "../../extras/consoleTable";
 
 export default function objectMessages(value: object): ConsoleMessage[] {
-    const messages: ConsoleMessage[] = [consoleText("{ ")];
-
-    let isFirst = true;
-    for (const key in value) {
-        if (value.hasOwnProperty(key)) {
-            if (isFirst) {
-                isFirst = false;
-            } else {
-                messages.push(consoleText(", "));
-            }
-            messages.push(consoleText(key, consoleStyles.collapsedObjectKey));
-            messages.push(consoleText(": "));
-            messages.push(...valueMessages(value[key as keyof typeof value]));
-        }
-    }
-
-    messages.push(consoleText(" }"));
-
-    return messages;
+    return consoleTable(value);
 }
+
+// function multiLineObject(value: object): ConsoleMessage[] {
+//     const pad = maxKeyLength(value)
+//     const messages: ConsoleMessage[] = []
+//     for (const key in value) {
+//         // if (value.hasOwnProperty(key)) {
+//         messages.push(consoleText(key.padEnd(pad), consoleStyles.collapsedObjectKey))
+//         messages.push(consoleText(' : '))
+//         messages.push(...valueMessages(value[key as keyof typeof value]))
+//         messages.push(consoleText('\n'))
+//         // }
+//     }
+//     return messages
+// }
+
+// function singleLineObject(value: object): ConsoleMessage[] {
+//     const messages: ConsoleMessage[] = [consoleText('{ ')]
+//
+//     let isFirst = true
+//     for (const key in value) {
+//         // if (value.hasOwnProperty(key)) {
+//             if (isFirst) {
+//                 isFirst = false
+//             } else {
+//                 messages.push(consoleText(', '))
+//             }
+//             messages.push(consoleText(key, consoleStyles.collapsedObjectKey))
+//             messages.push(consoleText(': '))
+//             messages.push(...valueMessages(value[key as keyof typeof value]))
+//         // }
+//     }
+//
+//     messages.push(consoleText(' }'))
+//
+//     return messages
+// }
+
+// function maxKeyLength(object: object): number {
+//     let max = 0
+//     for (const key in object) {
+//         if (object.hasOwnProperty(key)) {
+//             max = Math.max(max, key.length)
+//         }
+//     }
+//     return max
+// }
