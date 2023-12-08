b120e070b0955c8f60419646faf826a70aceb68b
Antonio Stoilkov <hello@astoilkov.com>
2023-12-08 15:40:41 +0200

    🎨 prettier

--- index.ts
+++ index.ts
@@ -1,10 +1,10 @@
 // core
-export { default as consolePrint } from './src/core/consolePrint'
-export type { default as ConsoleMessage } from './src/core/ConsoleMessage'
-export { consoleText } from './src/core/consoleText'
-export type { ConsoleText } from './src/core/consoleText'
-export { consoleObject } from './src/core/consoleObject'
-export type { ConsoleObject } from './src/core/consoleObject'
-export { consoleLine } from './src/core/consoleLine'
-export { consoleGroup } from './src/core/consoleGroup'
-export type { ConsoleGroup } from './src/core/consoleGroup'
+export { default as consolePrint } from "./src/core/consolePrint";
+export type { default as ConsoleMessage } from "./src/core/ConsoleMessage";
+export { consoleText } from "./src/core/consoleText";
+export type { ConsoleText } from "./src/core/consoleText";
+export { consoleObject } from "./src/core/consoleObject";
+export type { ConsoleObject } from "./src/core/consoleObject";
+export { consoleLine } from "./src/core/consoleLine";
+export { consoleGroup } from "./src/core/consoleGroup";
+export type { ConsoleGroup } from "./src/core/consoleGroup";
