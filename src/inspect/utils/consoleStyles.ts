1d3810563304df82e19aa3a7f0eae8b3cab8e6ae
Antonio Stoilkov <hello@astoilkov.com>
2023-12-19 18:33:27 +0200

    🐛 undefined & null color isn't the same as in DevTools

--- src/inspect/utils/consoleStyles.ts
+++ src/inspect/utils/consoleStyles.ts
@@ -1,7 +1,7 @@
 const consoleStyles = {
     string: { color: "#CC0100" },
     number: { color: "#1A1AA6" },
-    undefinedNull: { color: "#888" },
+    undefinedNull: { color: "#AAAAAA" },
     collapsedObjectKey: { color: "#5F6367" },
     expandedKey: { color: "#881180", fontWeight: "bold" },
 };
