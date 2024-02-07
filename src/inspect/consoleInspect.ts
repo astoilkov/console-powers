10e92e7d00cf09681158561d6c2f10786c30b562
Antonio Stoilkov <hello@astoilkov.com>
2024-02-07 19:22:18 +0200

    ♻️ extract defaultLineLength() as a util function

--- src/inspect/consoleInspect.ts
+++ src/inspect/consoleInspect.ts
@@ -2,6 +2,7 @@ import consolePrint from "../core/consolePrint";
 import inspectAny from "./inspectors/inspectAny";
 import consoleApply from "../core/consoleApply";
 import ConsoleSpan from "../core/ConsoleSpan";
+import defaultLineLength from "../utils/defaultLineLength";
 
 export interface ConsoleInspectOptions {
     line?: boolean;
@@ -53,16 +54,4 @@ export default function consoleInspect(
     }
 
     return spans;
-}
-
-function defaultLineLength(): number {
-    // - if a window is 1400px wide, a DevTools window positioned horizontally
-    //   will have around 1150 space for printing characters on one line. note
-    //   that this is variable due to the right side being taken by the name of
-    //   the function that called logging which varies in length
-    // - then, you have around 250px of margins that DevTools is adding
-    // - then, each character is around 6.6px wide
-    const CHAR_WIDTH = 6.6;
-    const DEV_TOOLS_MARGINS = 250;
-    return Math.round((window.innerWidth - DEV_TOOLS_MARGINS) / CHAR_WIDTH);
 }
---
+++ src/utils/defaultLineLength.ts
@@ -0,0 +1,11 @@
+export default function defaultLineLength(): number {
+    // - if a window is 1400px wide, a DevTools window positioned horizontally
+    //   will have around 1150 space for printing characters on one line. note
+    //   that this is variable due to the right side being taken by the name of
+    //   the function that called logging which varies in length
+    // - then, you have around 250px of margins that DevTools is adding
+    // - then, each character is around 6.6px wide
+    const CHAR_WIDTH = 6.6;
+    const DEV_TOOLS_MARGINS = 250;
+    return Math.round((window.innerWidth - DEV_TOOLS_MARGINS) / CHAR_WIDTH);
+}
