97d70b5e3f286a93caffdf2901d4e1fb30064b64
Antonio Stoilkov <hello@astoilkov.com>
2024-12-19 13:32:00 +0200

    🐛 could not resolve "./src/global/typings"

--- global.ts
+++ global.ts
@@ -1,3 +1,3 @@
-import './src/global/typings'
+import './src/global/typings.d.ts'
 export { default as addToGlobalScope } from "./src/global/addToGlobalScope";
 export { default as addNoopToGlobalScope } from "./src/global/addNoopToGlobalScope";
