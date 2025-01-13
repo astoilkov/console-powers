09b43b0b776fc9c14d05ee3b3c541c177275e5e9
Antonio Stoilkov <hello@astoilkov.com>
2025-01-13 13:29:22 +0200

    🐛 expand groups by default

--- src/inspect/consoleInspect.ts
+++ src/inspect/consoleInspect.ts
@@ -99,6 +99,7 @@ function inspect(
                 if (Array.isArray(value) || isIterable(value)) {
                     return [
                         consoleGroup({
+                            expanded: options.depth > 0,
                             header: inspection.spans,
                             body: inspectIterableMultiLine(
                                 makeIterableDetails(value),
@@ -110,6 +111,7 @@ function inspect(
                 } else if (isPlainObject(value)) {
                     return [
                         consoleGroup({
+                            expanded: options.depth > 0,
                             header: inspection.spans,
                             body: inspectObjectMultiLine(
                                 value,
