3f23a8e2353927005566184692584483b847f8ed
Antonio Stoilkov <hello@astoilkov.com>
2025-01-13 17:54:33 +0200

    🧪 fix `keys()` example

--- playground/playground.ts
+++ playground/playground.ts
@@ -296,32 +296,26 @@ examples.make(() => {
 
 examples.make(() => {
     // options.keys
-    ii(
-        [
-            {
-                model: 'MacBook Air 13"',
-                year: new Date(2020, 10, 23),
-                price: { currency: "USD", amount: 999 },
-                owner: "astoilkov",
-            },
-            {
-                model: 'MacBook Air 15"',
-                year: new Date(2023, 9, 18),
-                price: { currency: "USD", amount: 1299 },
-                owner: "erusev",
-            },
-            {
-                model: 'MacBook Pro 13"',
-                year: new Date(2019, 11, 2),
-                price: { currency: "USD", amount: 1499 },
-                owner: "astoilkov",
-            },
-        ],
+    ii.depth(3).keys("price", "year", "amount")([
         {
-            depth: 3,
-            keys: ["price", "year", "amount"],
+            model: 'MacBook Air 13"',
+            year: new Date(2020, 10, 23),
+            price: { currency: "USD", amount: 999 },
+            owner: "astoilkov",
         },
-    );
+        {
+            model: 'MacBook Air 15"',
+            year: new Date(2023, 9, 18),
+            price: { currency: "USD", amount: 1299 },
+            owner: "erusev",
+        },
+        {
+            model: 'MacBook Pro 13"',
+            year: new Date(2019, 11, 2),
+            price: { currency: "USD", amount: 1499 },
+            owner: "astoilkov",
+        },
+    ]);
 });
 
 examples.make(() => {
